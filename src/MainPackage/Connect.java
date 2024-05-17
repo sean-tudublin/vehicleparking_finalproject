3391c7db44e6d3b1d59b3042fcc1b7d8688622c9
sean <sean@sean-C20411614>
2024-05-17 05:56:27 +0100

    Second commit

--- src/MainPackage/Connect.java
+++ src/MainPackage/Connect.java
@@ -8,7 +8,6 @@ public class Connect {
         Connection con = null;
         try {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        //Username and password
         con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/vehicleparkingDB","sean","1234"); 
         System.out.println("Database connected");
         }
